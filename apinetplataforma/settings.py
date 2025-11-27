6cab4d3d86624615a5742573ba4313db15468349
juan camilo <camilodev1990@gmail.com>
2025-11-27 11:30:48 -0500

    modifico settings

--- apinetplataforma/settings.py
+++ apinetplataforma/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = [host.strip() for host in os.getenv('ALLOWED_HOSTS', '').split(',') if host.strip()]
 
 
 INSTALLED_APPS = [
